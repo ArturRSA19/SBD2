93c960bb3dd134e3781879c2d49ec1bb4dae32ef
Artur Rodrigues <artur190503@outlook.com>
2023-09-04 18:49:03 -0300

    Exercício 1 da Aula 4

---
+++ 1 - Revisão em Bancos de Dados/Exercícios/aula4extra1_ArturAlves_211043638/aula4extra1Fisico_ArturAlves_211043638.sql
@@ -0,0 +1,36 @@
+﻿-- -------- < aula4extra1 > --------
+--
+--                    SCRIPT DE CRIACAO (DDL)
+--
+-- Data Criacao ...........: 04/09/2023
+-- Autor(es) ..............: Artur Rodrigues Sousa Alves
+-- Banco de Dados .........: MySQL 8.0
+-- Base de Dados (nome) ...: aula4extra1
+--
+-- PROJETO => 01 Base de Dados
+--         => 02 Tabelas
+-- 
+-- Ultimas Alteracoes
+--   04/09/2023 => Criação das tabelas e da base de dados.
+--
+-- ---------------------------------------------------------
+
+CREATE DATABASE IF NOT EXISTS aula4extra1;
+USE aula4extra1;
+
+CREATE TABLE ESTADO (
+    sigla char(2) NOT NULL,
+    nome char(20) NOT NULL,
+    CONSTRAINT ESTADO_PK PRIMARY KEY (sigla)
+) ENGINE = INNODB;
+
+CREATE TABLE CIDADE (
+    codigo int NOT NULL AUTO_INCREMENT,
+    nome char(50) NOT NULL,
+    habitantes int NOT NULL,
+    sigla char(2) NOT NULL,
+    CONSTRAINT CIDADE_PK PRIMARY KEY (codigo),
+    CONSTRAINT CIDADE_ESTADO_FK FOREIGN KEY (sigla) REFERENCES ESTADO (sigla),
+    CONSTRAINT CIDADE_UK UNIQUE (sigla, nome), -- Chave Candidata (UK) para Sigla e Nome da Cidade
+    CONSTRAINT CIDADE_HABITANTES_CK CHECK (habitantes > 1000) -- Restrição de Verificação (CK) para Habitantes maiores que mil
+) ENGINE = INNODB;
---
+++ 1 - Revisão em Bancos de Dados/Exercícios/aula4extra1_ArturAlves_211043638/aula4extra1Popula_ArturAlves_211043638.sql
@@ -0,0 +1,36 @@
+﻿-- -------- < aula4extra1 > --------
+--
+--                    SCRIPT (DML)
+--
+-- Data Criacao ...........: 04/09/2023
+-- Autor(es) ..............: Artur Rodrigues Sousa Alves
+-- Banco de Dados .........: MySQL 8.0
+-- Base de Dados (nome) ...: aula4extra1
+--
+-- PROJETO => 01 Base de Dados
+--         => 02 Tabelas
+-- 
+-- Ultimas Alteracoes
+--   04/09/2023 => Inserindo dados nas tabelas ESTADO e CIDADE.
+--
+-- ---------------------------------------------------------
+
+USE aula4extra1;
+
+INSERT INTO ESTADO VALUES
+	('AC', 'Acre'),
+    ('DF', 'Brasília'), 
+    ('GO', 'Goiás'),
+    ('MS', 'Mato Grosso do Sul');
+    
+INSERT INTO CIDADE VALUES
+	(1, 'Rio Branco', 50000, 'AC'),
+    (2, 'Cruzeiro do Sul', 87446, 'AC'),
+    (3, 'Brasília', 3053160, 'DF'),
+    (4, 'Águas Claras', 228770, 'DF'),
+    (5, 'Taguatinga', 325639, 'DF'),
+    (6, 'Novo Gama', 100000, 'GO'),
+    (7, 'Águas Lindas', 130000, 'GO'),
+    (8, 'Maracaju', 60000, 'MS'),
+	(9, 'Campo Grande', 813000, 'MS'),
+    (10, 'Três Lagoas', 143000, 'MS');
